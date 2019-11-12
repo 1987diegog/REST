11562019bfa7132338183205076e65080ffc65fc
DiegoG09 <1987diegog@gmail.com>
2019-11-12 14:47:49 -0300

    RESTeasy-jaxrs is defined with <scope>provided</scope> so that JBoss will provide this dependency in runtime

--- src/main/java/uy/com/demente/ideas/services/RESTServices.java
+++ src/main/java/uy/com/demente/ideas/services/RESTServices.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -72,21 +71,6 @@ public class RESTServices {
 	@PUT
 	@Produces("application/json")
 	public String modifyPersonPUT(PersonDTO personDTO) {
-
-		System.out.println("Name: " + personDTO.getName());
-		System.out.println("LastName: " + personDTO.getLastName());
-		System.out.println("Age: " + personDTO.getAge());
-
-		personServices.modifyPerson(personDTO);
-		System.out.println("Persona modificada con exito");
-		return "OK";
-	}
-
-	/**
-	 */
-	@PATCH
-	@Produces("application/json")
-	public String modifyPersonPATCH(PersonDTO personDTO) {
 
 		System.out.println("Name: " + personDTO.getName());
 		System.out.println("LastName: " + personDTO.getLastName());
